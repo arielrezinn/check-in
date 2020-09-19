b914d0db5aaf38a8649d9429e722b801189ff85e
arielrezinn <arielrezin@gmail.com>
2020-09-19 10:19:45 -0500

    roughhhh beginning w/ sidebar

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
-import React from 'react';
-import { Box, Grommet, Nav } from 'grommet';
-import { Sidebar } from 'grommet-controls';
+import React, { useState } from 'react';
+import {
+  Box,
+  grommet,
+  Grommet,
+} from 'grommet';
 
-const theme = {
-  global: {
-    font: {
-      family: 'Futura',
-      size: '18px',
-      height: '20px',
-    },
-  },
-};
+import { TooltipsSidebar } from './components/sidebar';
 
 function App() {
   return (
-    <Grommet theme={theme} themeMode="dark">
-      <Sidebar title='My title'>
-
-      </Sidebar>
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Grommet theme={grommet} themeMode="light" full>
+      <Box direction="row" flex overflow={{ horizontal: 'hidden'}} fill>
+        <TooltipsSidebar/>
+        <Box flex align='center' justify='center'>
+          app body
+        </Box>
+    </Box>
     </Grommet>
   );
 }
