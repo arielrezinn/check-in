42eb2b5a70ae000c8bc904cd2fa97c71abbb22cf
Ruiran Xun <xun.ruiran@gmail.com>
2020-09-19 19:07:33 -0400

    hotfix to formatting

--- src/App.js
+++ src/App.js
@@ -12,11 +12,6 @@ import { Cards } from './components/cards'
 function App() {
   return (
     <Grommet theme={grommet} themeMode="light" full>
-      <Box direction="row" flex overflow={{ horizontal: 'hidden'}} fill>
-        <Navbar/>
-        <Box flex align='center' justify='center'>
-          app body
-        </Box>
       <Box pad="medium" fill>
         <Tabs flex>
           <Tab title='Check-In'>
@@ -28,6 +23,11 @@ function App() {
           <Tab title='Stats'>
           </Tab>
         </Tabs>
+      </Box>
+      <Box direction="row" flex overflow={{ horizontal: 'hidden'}} fill>
+        <Box flex align='center' justify='center'>
+          app body
+        </Box>
       </Box>
     </Grommet>
   );
