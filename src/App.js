cbc745c25c60d57dc1bed3f141087c52726a004a
arielrezinn <arielrezin@gmail.com>
2020-09-19 19:03:38 -0500

    changes yay!

--- src/App.js
+++ src/App.js
@@ -7,29 +7,23 @@ import {
   Tabs
 } from 'grommet';
 import { CalendarWindow } from './components/calendar'
-import { Cards } from './components/cards'
 import { EmojiWindow } from './components/emojiview'
 
 function App() {
   return (
     <Grommet theme={grommet} themeMode="light" full>
-      <Box pad="medium" fill>
+      <Box direction="row" flex overflow={{ horizontal: 'hidden'}} pad="medium" fill>
         <Tabs flex>
           <Tab title='Check-In'>
-
+            <EmojiWindow></EmojiWindow>
           </Tab>
           <Tab title='Overview'>
             <CalendarWindow></CalendarWindow>
           </Tab>
           <Tab title='Stats'>
-            <EmojiWindow></EmojiWindow>
+            
           </Tab>
         </Tabs>
-      </Box>
-      <Box direction="row" flex overflow={{ horizontal: 'hidden'}} fill>
-        <Box flex align='center' justify='center'>
-          app body
-        </Box>
       </Box>
     </Grommet>
   );
--- src/components/emojiview.js
+++ src/components/emojiview.js
@@ -1,56 +1,44 @@
 import React from 'react'
-import { Grommet, Box, Header, Heading, Text } from 'grommet'
-
-const theme = {
-  "global": {
-    "colors": {
-      "background": {
-        "light": "#ffffff",
-        "dark": "#000000"
-      }
-    },
-    "font": {
-      "family": "-apple-system,\n         BlinkMacSystemFont, \n         \"Segoe UI\", \n         Roboto, \n         Oxygen, \n         Ubuntu, \n         Cantarell, \n         \"Fira Sans\", \n         \"Droid Sans\",  \n         \"Helvetica Neue\", \n         Arial, sans-serif,  \n         \"Apple Color Emoji\", \n         \"Segoe UI Emoji\", \n         \"Segoe UI Symbol\""
-    }
-  },
-  "button": {
-    "extend": [
-      null
-    ]
-  }
-}
+import { Box, Card, CardBody, Header, Heading, Text } from 'grommet'
 
 export class EmojiWindow extends React.Component {
     constructor() {
         super();
         // todo
     }
+
+    function 
     render() {
         return (
-            <Box fill="vertical" overflow="auto" align="center" flex="grow" direction="column">
+          <Box fill="vertical" overflow="auto" align="center" flex="grow" direction="column">
             <Header align="center" direction="row" flex={false} justify="between" gap="medium">
                 <Heading level="2">
                 How are you doing today?
                 </Heading>
             </Header>
-            <Box align="center" justify="center" direction="row">
-                <Box align="center" justify="center" pad="large">
-                <Text size="xxlarge">
-                    😊
-                </Text>
-                </Box>
-                <Box align="center" justify="center" pad="large">
-                <Text size="xxlarge">
-                    😑
-                </Text>
-                </Box>
-                <Box align="center" justify="center" pad="large">
-                <Text size="xxlarge">
-                    😢
-                </Text>
-                </Box>
+            <Box align="center" justify="center" direction="row" gap='large'>
+                <Card 
+                  pad='large'
+                  onClick={() => {
+              
+                    alert('Card was Clicked!');
+                  }}>
+                  <Text size="xxlarge">
+                      😊
+                  </Text>
+                </Card>
+                <Card pad='large'>
+                  <Text size="xxlarge">
+                      😑
+                  </Text>
+                </Card>
+                <Card pad='large'>
+                  <Text size="xxlarge">
+                      😢
+                  </Text>
+                </Card>
             </Box>
-            </Box>
+          </Box>
         )
     }
 }
