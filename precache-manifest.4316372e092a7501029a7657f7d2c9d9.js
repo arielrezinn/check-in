71d89db02baa7345de72ebcdeabdd14eeffed1a1
arielrezinn <arielrezin@gmail.com>
2020-09-20 07:28:03 -0500

    Updates

---
+++ precache-manifest.4316372e092a7501029a7657f7d2c9d9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1f0b331a3008f90f02d57fcb35754c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4e047c16a396346381a4",
+    "url": "/static/css/2.32a52a2a.chunk.css"
+  },
+  {
+    "revision": "4e047c16a396346381a4",
+    "url": "/static/js/2.258aaed7.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/static/js/2.258aaed7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0ca945798db9dd2d76e6",
+    "url": "/static/js/main.f2202be3.chunk.js"
+  },
+  {
+    "revision": "69763547c781d2a1dc02",
+    "url": "/static/js/runtime-main.b91f4a23.js"
+  }
+]);
